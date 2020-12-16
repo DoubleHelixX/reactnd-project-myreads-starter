cbe2ec8576c7b3280349a55c7c490383f188d6e3
DoubleHelix <tjrnyc5@gmail.com>
2020-12-15 21:01:01 -0500

    reated map func with props to load elements on screen based on data directly for current reads(books) & added validation checks to make sure each data element isnt broken by each other

--- src/App.js
+++ src/App.js
@@ -16,8 +16,17 @@ class BooksApp extends React.Component {
      */
     showSearchPage: false,
 
+    shelf1: {
+      'id': [1,2],
+      'urls': [
+        "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+        "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
+      ],
+      'titles': ['To Kill a Mockingbird', "Ender's Game"],
+      'authors': ["Harper Lee", "Orson Scott Card"]
+  },
     shelf2: {
-      'id': [4,5],
+      'id': [3,4],
       'urls': [
         "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
         "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
@@ -26,7 +35,7 @@ class BooksApp extends React.Component {
       'authors': ["David McCullough", "J.K. Rowling"]
   },
     shelf3: {
-      'id': [6,7,8],
+      'id': [4,5,6],
       'urls': [
         "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
         "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
@@ -71,7 +80,7 @@ class BooksApp extends React.Component {
             <div className="list-books-content">
               <div>
 
-                <ListCurrentBooks  /> 
+                <ListCurrentBooks shelf1={this.state.shelf1} /> 
                 <ListWantedBooks shelf2={this.state.shelf2}/>
                 <ListReadableBooks shelf3={this.state.shelf3} />
                
--- src/ListCurrentBooks.js
+++ src/ListCurrentBooks.js
@@ -4,54 +4,70 @@ import { Link } from 'react-router-dom'
 
 
 class ListCurrentBooks extends Component {
+    static propTypes = {
+        shelf1: PropTypes.shape({
+            'id': PropTypes.array.isRequired,
+            'urls': PropTypes.array.isRequired,
+            'titles': PropTypes.array.isRequired,
+            'authors': PropTypes.array.isRequired
+
+
+      })
+      }
+
+      
+    
     render() {
-        
+    const {shelf1} = this.props
+    let prevKey='';
+    const titles_length= shelf1.titles.length;
+    const authors_length= shelf1.authors.length;
+    const urls_length= shelf1.urls.length;
+
+
+
+    Object.keys(shelf1).map((key, index)=> {
+        if (index <= 0)
+            prevKey= key;
+        else 
+        {
+            if (shelf1[key].length > shelf1[prevKey].length)
+                prevKey= key;
+        }
+        return '';
+    })
+
         return (
         <div className="bookshelf">
             <h2 className="bookshelf-title">Currently Reading</h2>
             <div className="bookshelf-books">
             <ol className="books-grid">
-                <li>
-                <div className="book">
-                    <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}>
-                    </div> 
-                    <div className="book-shelf-changer">
-                        <select>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                        </select>
+                {shelf1[prevKey].map((name,i) =>(
+                    <li key ={shelf1.id[i]}>
+                    <div className="book">
+                        <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${i<=urls_length-1 ? shelf1.urls[i]: '' })` }}>
+                        </div> 
+                        <div className="book-shelf-changer">
+                            <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                            </select>
+                        </div>
+                        </div>
+                        <div className="book-title">  {i<=titles_length-1 ? shelf1.titles[i] : ''}   </div>
+                        <div className="book-authors"> {i<=authors_length-1 ? shelf1.titles[i] : ''}  </div>
                     </div>
-                    </div>
-                    <div className="book-title">To Kill a Mockingbird</div>
-                    <div className="book-authors">Harper Lee</div>
-                </div>
-                </li>
-                <li>
-                <div className="book">
-                    <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}>    
-                    </div>
-                    <div className="book-shelf-changer">
-                        <select>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                        </select>
-                    </div>
-                    </div>
-                    <div className="book-title">Ender's Game</div>
-                    <div className="book-authors">Orson Scott Card</div>
-                </div>
-                </li>
+                    </li>
+                       ))}
             </ol>
             </div>
         </div>
+         
+
             )
     }
 }
