fc6f0a70fa17d5f13f45c4a8aa1b466019f183ea
DoubleHelix <tjrnyc5@gmail.com>
2020-12-19 22:41:23 -0500

    added looping func for li filter list

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -63,6 +63,8 @@ class SearchBooks extends Component {
             'shelf': ['tempHolder'],
             'id' :['tempHolder']
         };
+        let searchableTerms = ['Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'
+    ];
         books.map((b) =>{ 
             if (query.length>1)
             {
@@ -115,106 +117,9 @@ class SearchBooks extends Component {
                                 <div className="dropdown">
                                     <button className="dropbtn">Filter</button>
                                     <div className="dropdown-content">
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Android</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}> Art</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Artificial Intelligence</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Astronomy</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Austen</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Baseball</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Basketball</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Bhagat</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Biography</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Brief</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Business</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Camus</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Cervantes</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Christie</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Classics</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Comics</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Cook</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Android</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}> Art</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Artificial Intelligence</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Astronomy</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Austen</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Baseball</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Basketball</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Bhagat</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Biography</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Brief</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Business</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Camus</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Cervantes</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Christie</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Classics</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Comics</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Cook</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Cricket</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Cycling</li> 
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Desai</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Design</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Development</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Digital</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Marketing</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Drama</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Drawing</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Dumas</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Education</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Everything</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Fantasy</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Film</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Finance</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>First</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Fitness</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Football</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Future</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Games</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Gandhi</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Homer</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Horror</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Hugo</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Ibsen</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Journey</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Kafka</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>King</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Lahiri</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Larsson</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Learn</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Literary</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Make</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Manage</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Marquez</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Money</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Mystery</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Negotiate</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Painting</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Philosophy</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Photography</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Poetry</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Production</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Programming</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>React</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Redux</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>River</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Robotics</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Rowling</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Satire</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Science</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Fiction</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Shakespeare</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Singh</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Swimming</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Tale</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Thrun</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Time</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Tolstoy</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Travel</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Ultimate</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Virtual</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Reality</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Web</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>Development</li>
-                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}>iOS</li>
+                                        {searchableTerms.map((genre) => (
+                                        <li onClick= {(event) => this.searchBooks(event.target.innerText ? event.target.innerText : '@_____@')}> {genre}</li> 
+                                        ))}
                                     </div>
                                 </div>
 
